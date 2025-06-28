e981d3afaed9450a5ed4ea618cd5d8f1621997e0
satyam0777 <officialsatyam0777@gmail.com>
2025-06-29 01:31:40 +0530

    dom manipulation concept

---
+++ 07-dom-manipulation.js
@@ -0,0 +1,21 @@
+// DOM MANIPULATION (to run in browser console)
+
+// Selecting elements
+// const btn = document.getElementById('myButton');
+// const items = document.querySelectorAll('.item');
+
+// Changing content
+// btn.textContent = "Click Me!";
+
+// Adding event listener
+// btn.addEventListener('click', function() {
+//     alert('Button clicked!');
+// });
+
+// Creating and appending elements
+// const newDiv = document.createElement('div');
+// newDiv.textContent = "Hello from JS!";
+// document.body.appendChild(newDiv);
+
+// Removing elements
+// newDiv.remove();
